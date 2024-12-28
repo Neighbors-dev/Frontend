5e50a0ca33f11270ba7e37ba6fa997aa5809a09c
AAminha <dks4857@gmail.com>
2024-12-28 21:30:09 +0900

    style: 전역 스타일 변경으로 인한 스타일 코드 수정

--- src/pages/NicknameComplete.tsx
+++ src/pages/NicknameComplete.tsx
@@ -1,11 +1,13 @@
 import { ArrowLeftIcon } from '@/assets'
 import SolidButton from '@/components/SolidButton'
+import useBodyBackgroundColor from '@/hooks/useBodyBackgroundColor'
 import { getSessionNickname } from '@/utils/nicknameUtils'
 import { Navigate, useNavigate } from 'react-router-dom'
 
 export default function NicknameComplete() {
   const nickname = getSessionNickname()
   const navigate = useNavigate()
+  useBodyBackgroundColor('neutral-90')
 
   /* useEffect(() => {
     return () => removeSessionNickname()
@@ -14,34 +16,32 @@ export default function NicknameComplete() {
   if (!nickname) return <Navigate to="/nickname" replace />
 
   return (
-    <div className="full-height flex flex-col bg-neutral-90">
+    <main className="flex w-full flex-col">
       <header className="max-w-600 px-5 py-3">
         <button type="button" onClick={() => navigate('/nickname')}>
-          <ArrowLeftIcon className="text-white" />
+          <ArrowLeftIcon className="h-6 w-6 text-white" />
         </button>
       </header>
-      <main className="max-w-600 grow px-5 pb-[5%] pt-[7%]">
-        <div className="flex h-full w-full flex-col justify-between">
-          <section className="w-full text-center">
-            <h1 className="headline-small mb-4 text-white">반가워요, {nickname} 님!</h1>
-            <h2 className="body-large text-neutral-30">메시지를 작성해 거리를 환하게 밝혀주세요</h2>
-          </section>
-          <div className="flex items-center justify-center bg-brand-yellow p-10">
-            일러스트 들어갈 자리
-          </div>
-          <SolidButton
-            variant="primary"
-            size="large"
-            type="button"
-            className=""
-            onClick={() => {
-              // TODO: 메시지 작성 페이지로 이동
-            }}
-          >
-            메시지 작성하러 가기
-          </SolidButton>
+      <div className="flex grow flex-col justify-between px-5 pb-[5%] pt-[7%]">
+        <section className="w-full text-center">
+          <h1 className="headline-small mb-4 text-white">반가워요, {nickname} 님!</h1>
+          <h2 className="body-large text-neutral-30">메시지를 작성해 거리를 환하게 밝혀주세요</h2>
+        </section>
+        <div className="flex h-[280px] items-center justify-center bg-brand-yellow px-10">
+          일러스트 들어갈 자리
         </div>
-      </main>
-    </div>
+        <SolidButton
+          variant="primary"
+          size="large"
+          type="button"
+          className=""
+          onClick={() => {
+            // TODO: 메시지 작성 페이지로 이동
+          }}
+        >
+          메시지 작성하러 가기
+        </SolidButton>
+      </div>
+    </main>
   )
 }

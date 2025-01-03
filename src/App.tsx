832626b8a0b444dcdfa4c10f4555365483b416b0
AAminha <dks4857@gmail.com>
2025-01-04 01:03:11 +0900

    feat: Notice 페이지 구현 및 NoticeItem 컴포넌트 구현

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Main from '@/pages/Main'
 import Nickname from '@/pages/Nickname'
 import NicknameComplete from '@/pages/NicknameComplete'
 import useViewportHeight from '@/hooks/useViewportHeight'
+import Notice from './pages/Notice'
 
 export default function App() {
   useViewportHeight()
@@ -16,6 +17,7 @@ export default function App() {
       <Route path="callback/kakaotalk" element={<AuthCallback />} />
       <Route path="nickname" element={<Nickname />} />
       <Route path="nickname-complete" element={<NicknameComplete />} />
+      <Route path="notice" element={<Notice />} />
     </Routes>
   )
 }
---
+++ src/pages/Notice.tsx
@@ -0,0 +1,36 @@
+import { getNotices } from '@/apis/notice'
+import { ArrowLeftIcon } from '@/assets'
+import NoticeItem from '@/containers/Notice/NoticeItem'
+import useBodyBackgroundColor from '@/hooks/useBodyBackgroundColor'
+import { useEffect, useState } from 'react'
+
+export default function Notice() {
+  const [notices, setNotices] = useState<NoticeType[]>([])
+  useBodyBackgroundColor('neutral-90')
+
+  useEffect(() => {
+    const fetchNotices = async () => {
+      const result = await getNotices()
+      if (result) setNotices(result.notices)
+    }
+
+    fetchNotices()
+  }, [])
+
+  return (
+    <main className="flex w-full flex-col">
+      <header className="max-w-600 fixed flex items-center justify-between bg-neutral-90 px-5 py-3">
+        <button type="button" onClick={() => {}}>
+          <ArrowLeftIcon className="h-6 w-6 text-white" />
+        </button>
+        <p className="title-medium text-white">공지사항</p>
+        <div className="h-6 w-6" />
+      </header>
+      <div className="mx-5 mb-5 mt-12 flex grow flex-col gap-3">
+        {notices.map((notice) => (
+          <NoticeItem key={notice.id} notice={notice} />
+        ))}
+      </div>
+    </main>
+  )
+}

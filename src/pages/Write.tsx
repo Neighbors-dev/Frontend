8ee8940a5bb1b8e6ce8afd2a7143080a5cd80012
AAminha <dks4857@gmail.com>
2025-01-03 22:46:25 +0900

    feat: SelectTarget에 다음 단계 버튼 기능 추가 및 Write 페이지의 퍼널로 추가

--- src/pages/Write.tsx
+++ src/pages/Write.tsx
@@ -1,31 +1,53 @@
+import Header from '@/components/Header'
+import PrevButton from '@/components/PrevButton'
+import SelectTarget from '@/containers/SelectTarget'
+import useBodyBackgroundColor from '@/hooks/useBodyBackgroundColor'
 import useFunnel from '@/hooks/useFunnel'
 
+const WRITE_STEPS = ['select-target', 'second', 'third', 'fourth']
+
 export default function Write() {
-  const { Funnel, Step, setStep } = useFunnel('first')
+  const { Funnel, Step, setPrevStep, setNextStep } = useFunnel(WRITE_STEPS[0])
+  useBodyBackgroundColor('neutral-90')
+
   return (
-    <Funnel>
-      <Step name="first">
-        <div className="bg-red-300">
-          <button onClick={() => setStep('second')}>다음</button>
-        </div>
-      </Step>
-      <Step name="second">
-        <div className="bg-yellow-300">
-          <button onClick={() => setStep('first')}>이전</button>
-          <button onClick={() => setStep('third')}>다음</button>
-        </div>
-      </Step>
-      <Step name="third">
-        <div className="bg-green-300">
-          <button onClick={() => setStep('second')}>이전</button>
-          <button onClick={() => setStep('fourth')}>다음</button>
-        </div>
-      </Step>
-      <Step name="fourth">
-        <div className="bg-blue-300">
-          <button onClick={() => setStep('third')}>이전</button>
-        </div>
-      </Step>
-    </Funnel>
+    <main className="flex w-full flex-col">
+      <Header>
+        <PrevButton onClick={setPrevStep} />
+      </Header>
+      <Funnel>
+        <Step name={WRITE_STEPS[0]}>
+          <SelectTarget
+            nextButtonOnClick={(selected) => {
+              if (selected === 'specific') {
+                setNextStep(WRITE_STEPS[1])
+              } else {
+                setNextStep(WRITE_STEPS[3])
+              }
+            }}
+          />
+        </Step>
+        <Step name={WRITE_STEPS[1]}>
+          <div className="bg-yellow-300">
+            <h1>1단계</h1>
+            <button onClick={() => setNextStep('first')}>이전</button>
+            <button onClick={() => setNextStep('third')}>다음</button>
+          </div>
+        </Step>
+        <Step name={WRITE_STEPS[2]}>
+          <div className="bg-green-300">
+            <h1>2단계</h1>
+            <button onClick={() => setNextStep('second')}>이전</button>
+            <button onClick={() => setNextStep('fourth')}>다음</button>
+          </div>
+        </Step>
+        <Step name={WRITE_STEPS[3]}>
+          <div className="bg-blue-300">
+            <h1>3단계</h1>
+            <button onClick={() => setNextStep('third')}>이전</button>
+          </div>
+        </Step>
+      </Funnel>
+    </main>
   )
 }

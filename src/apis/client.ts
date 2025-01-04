a658b392aacd85ec9a61c24005fec2091f916078
AAminha <dks4857@gmail.com>
2025-01-04 20:48:42 +0900

    feat: 추후에 사용할 토큰 재발급 로직 추가

--- src/apis/client.ts
+++ src/apis/client.ts
@@ -1,3 +1,4 @@
+import useAuthStore from '@/stores/authStore'
 import axios from 'axios'
 
 export const mockingClient = axios.create({
@@ -8,23 +9,47 @@ export const client = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 })
 
-/* client.interceptors.request.use((config) => {
-  // TODO: 토큰 관리 방법 재설정
-  //const token = localStorage.getItem('token')
+client.interceptors.request.use((config) => {
+  const token = useAuthStore.getState().getAccessToken()
   if (token) {
     config.headers['Authorization'] = `Bearer ${token}`
   }
   return config
-}) */
+})
 
-/* client.interceptors.response.use(
-  (response) => {
-    return response
-  },
-  (error) => {
-    if (error.response.status === 401) {
-      // 리프레시 토큰으로 토큰 재발급
+/* let retry = false
+
+client.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config
+    if (error.response?.status === 401 && !retry) {
+      retry = true
+      const refreshToken = useAuthStore.getState().getRefreshToken()
+
+      try {
+        const { data } = await client.post('/refresh', {
+          refreshToken,
+        });
+        useAuthStore.getState().login({
+          accessToken: {
+            value: data.accessToken,
+            expiresIn: data.expiresIn + Date.now(),
+          },
+          refreshToken: {
+            value: data.refreshToken,
+            expiresIn: data.refreshTokenExpiresIn + Date.now(),
+          },
+        });
+
+        originalRequest.headers['Authorization'] = `Bearer ${data.accessToken}`;
+        return client(originalRequest);
+      } catch {
+        useAuthStore.getState().logout();
+        // 토큰 재발급 실패 시 로그아웃
+      } finally {
+        retry = false;
+      }
     }
-    return Promise.reject(error)
   }
 ) */

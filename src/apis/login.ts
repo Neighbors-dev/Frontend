4f7d2c1b6fd6d08bfb05700c08afdbc8a643995b
AAminha <dks4857@gmail.com>
2025-01-04 20:31:33 +0900

    fix: 로그인 api 로직 수정

--- src/apis/login.ts
+++ src/apis/login.ts
@@ -1,14 +1,44 @@
-import { mockingClient } from './client'
+import useAuthStore from '@/stores/authStore'
+import { client } from './client'
 
 export const postLogin = async (code: string) => {
+  const login = useAuthStore.getState().login
+  let api
+
+  if (process.env.NODE_ENV === 'development') {
+    api = '/oauth/kakao/callback2'
+  } else {
+    api = '/oauth/kakao/callback'
+  }
+
   try {
-    const { data } = await mockingClient.post('/mocks/login.json', {
-      code,
+    const {
+      data: {
+        data: { authTokens },
+      },
+    } = await client.get(api, {
+      params: {
+        code,
+      },
     })
-    return data
+
+    const token: LoginTokenType = {
+      accessToken: {
+        value: authTokens.accessToken,
+        expiresIn: authTokens.expiresIn + Date.now(),
+      },
+      refreshToken: {
+        value: authTokens.refreshToken,
+        expiresIn: authTokens.refreshTokenExpiresIn + Date.now(),
+      },
+    }
+    login(token)
+
+    return true
   } catch (error) {
     if (error instanceof Error) {
       console.error(error.message)
     }
+    return false
   }
 }

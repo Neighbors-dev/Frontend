8542ce8a2f0f706dfd9822fb8eb2bd450aa452db
AAminha <dks4857@gmail.com>
2025-01-11 00:51:47 +0900

    design: TextField 변경된 디자인 반영

--- src/components/TextField.tsx
+++ src/components/TextField.tsx
@@ -2,40 +2,42 @@ import { ComponentPropsWithoutRef } from 'react'
 import { twMerge } from 'tailwind-merge'
 
 interface TextFieldProps extends ComponentPropsWithoutRef<'input'> {
-  leftIcon?: React.ReactNode
-  rightIcon?: React.ReactNode
+  Icon?: React.FunctionComponent<React.SVGProps<SVGSVGElement>>
   className?: string
   onClick?: () => void
 }
 
-export default function TextField({
-  leftIcon,
-  rightIcon,
-  className,
-  onClick,
-  ...props
-}: TextFieldProps) {
+export default function TextField({ Icon, className, onClick, ...props }: TextFieldProps) {
   return (
     <div
       className={twMerge(
-        'flex w-full items-center gap-2 rounded-lg border border-transparent bg-neutral-80 p-4',
+        'group flex w-full items-center gap-2 rounded-lg border border-transparent bg-neutral-80 p-4',
         !onClick && 'focus-within:border-brand-yellow',
         className
       )}
       onClick={onClick}
-      role={onClick && 'button'}
+      role={!props.disabled && onClick ? 'button' : undefined}
     >
-      {leftIcon}
       <input
         type="text"
         readOnly={!!onClick}
         className={twMerge(
-          'label-large w-full bg-transparent text-white placeholder:text-neutral-50',
-          onClick && 'cursor-pointer'
+          'label-large w-full bg-transparent text-white placeholder:text-neutral-50 disabled:text-neutral-50',
+          !props.disabled && onClick && 'cursor-pointer'
         )}
         {...props}
       />
-      {rightIcon}
+      {Icon && (
+        <button type="button" disabled={props.disabled}>
+          <Icon
+            className={twMerge(
+              !onClick && 'group-focus-within:text-brand-yellow',
+              props.value ? 'text-neutral-30' : 'text-neutral-50',
+              props.disabled && 'text-neutral-50'
+            )}
+          />
+        </button>
+      )}
     </div>
   )
 }

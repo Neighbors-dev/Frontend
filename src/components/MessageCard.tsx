17fdaea783bb3f571602c2be2f941cfcbcc89345
AAminha <dks4857@gmail.com>
2025-01-14 03:52:28 +0900

    feat: 메시지 상세 조회 구현

--- src/components/MessageCard.tsx
+++ src/components/MessageCard.tsx
@@ -1,15 +1,32 @@
+import { twMerge } from 'tailwind-merge'
+
 interface MessageCardProps {
   message: MessageType
+  isShort: boolean
+  onClick?: () => void
 }
 
-export default function MessageCard({ message }: MessageCardProps) {
+export default function MessageCard({ message, isShort, onClick }: MessageCardProps) {
   return (
-    <article className="flex w-full flex-col gap-4 rounded-[20px] bg-message-gradient p-6">
+    <article
+      className={twMerge(
+        'flex w-full flex-col gap-4 rounded-[20px] bg-message-gradient p-6',
+        onClick && 'cursor-pointer'
+      )}
+      onClick={onClick}
+    >
       <h3 className="relative z-0 w-fit">
         <div className="absolute bottom-[1px] z-[-10] h-[11px] w-full bg-brand-yellow/60" />
         <p className="title-small text-neutral-80">To. {message.to}</p>
       </h3>
-      <p className="body-large line-clamp-3 whitespace-pre-wrap text-neutral-80">
+      <p
+        className={twMerge(
+          'body-large whitespace-pre-wrap text-neutral-80',
+          isShort && 'line-clamp-3'
+        )}
+      >
+        {message.content}
+        {message.content}
         {message.content}
       </p>
       <p className="title-small text-neutral-50">From. {message.from}</p>

1ba247019a4c8f0d42611e534acdb7e1c1d6dbc2
AAminha <dks4857@gmail.com>
2025-01-15 03:26:49 +0900

    design: 메시지 작성 비공개 여부 체크박스 추가

--- src/containers/Write/Funnel/WriteMessage.tsx
+++ src/containers/Write/Funnel/WriteMessage.tsx
@@ -1,3 +1,4 @@
+import Checkbox from '@/components/Checkbox'
 import SolidButton from '@/components/SolidButton'
 import { MESSAGE_MAX_LENGTH } from '@/constants/write'
 import useAuthStore from '@/stores/authStore'
@@ -9,6 +10,7 @@ import { useNavigate } from 'react-router-dom'
 export default function WriteMessage() {
   const navigate = useNavigate()
   const [content, setContent] = useState('')
+  const [isPrivate, setIsPrivate] = useState(false)
   const isLoggedIn = useAuthStore((state) => state.isLoggedIn)
   const nickname = useAuthStore((state) => state.user)?.nickname
   const toggleCollectionIntro = useWriteBottomStore((state) => state.toggleCollectionIntro)
@@ -37,7 +39,7 @@ export default function WriteMessage() {
 
   return (
     <div className="flex grow flex-col justify-between gap-10">
-      <section>
+      <section className="flex grow flex-col">
         <h2 className="headline-small mb-2 text-white">
           감사의 메시지를
           <br />
@@ -45,31 +47,44 @@ export default function WriteMessage() {
         </h2>
         <button
           type="button"
-          className="label-medium mb-7 text-neutral-40 underline underline-offset-2"
+          className="label-medium mb-7 text-left text-neutral-40 underline underline-offset-2"
           onClick={toggleCollectionIntro}
         >
           어떻게 작성해야 하나요?
         </button>
-        <section className="flex flex-col gap-4 rounded-[20px] bg-message-gradient p-6">
-          <h2 className="relative z-0 w-fit">
-            <div className="absolute bottom-[1px] z-[-10] h-[11px] w-full bg-brand-yellow/60" />
-            {/* TODO: 대상 선택 여부에 따라 다르게 보여지도록 */}
-            <p className="title-small text-neutral-80">To. {targetString}</p>
-          </h2>
-          <textarea
-            placeholder="내용을 입력해주세요."
-            className="body-large h-[204px] resize-none bg-transparent text-neutral-80 placeholder:text-neutral-50"
-            value={content}
-            onChange={handleMessageChange}
-          />
-          {/* TODO: 닉네임 받아서 넣기 */}
-          <div className="flex justify-between text-neutral-50">
-            <p className="title-small">From. {nickname}</p>
-            <p className="label-medium">
-              {content.length}/{MESSAGE_MAX_LENGTH}
-            </p>
-          </div>
-        </section>
+        <div className="flex grow flex-col">
+          <section className="flex max-h-[324px] grow flex-col gap-4 rounded-[20px] bg-message-gradient p-6">
+            <h2 className="relative z-0 w-fit">
+              <div className="absolute bottom-[1px] z-[-10] h-[11px] w-full bg-brand-yellow/60" />
+              {/* TODO: 대상 선택 여부에 따라 다르게 보여지도록 */}
+              <p className="title-small text-neutral-80">To. {targetString}</p>
+            </h2>
+            <textarea
+              placeholder="내용을 입력해주세요."
+              className="body-large grow resize-none bg-transparent text-neutral-80 placeholder:text-neutral-50"
+              value={content}
+              onChange={handleMessageChange}
+            />
+            {/* TODO: 닉네임 받아서 넣기 */}
+            <div className="flex justify-between text-neutral-50">
+              <p className="title-small">From. {nickname}</p>
+              <p className="label-medium">
+                {content.length}/{MESSAGE_MAX_LENGTH}
+              </p>
+            </div>
+          </section>
+          <label
+            htmlFor="isPrivate"
+            className="label-medium mt-4 flex cursor-pointer items-center gap-2 text-white"
+          >
+            <Checkbox
+              id="isPrivate"
+              checked={isPrivate}
+              onChange={() => setIsPrivate((prev) => !prev)}
+            />
+            <span>비공개로 작성하기</span>
+          </label>
+        </div>
       </section>
       <SolidButton
         variant="primary"

c6e52773e80330142e5cbba65dcb4c27a2550cde
AAminha <dks4857@gmail.com>
2025-01-17 20:01:10 +0900

    feat: 내가 작성한 메시지 데이터 컴포넌트에 반영

--- src/containers/Message/MyMessageList.tsx
+++ src/containers/Message/MyMessageList.tsx
@@ -1,14 +1,6 @@
 import MyMessageCard from './MyMessageCard'
-
-const TEMP = {
-  to: '용산파출소 김대헌 경관님',
-  from: 'sk',
-  content:
-    '안녕하세요 김대헌 경관님! 저번에 만취해서 전봇대에서 잠든 저를 구해주셔서 감사합니다! 항상 응원합니다!',
-}
-
 interface MessageListProps {
-  messages: MessageType[]
+  messages: MyMessageType[]
 }
 
 export default function MyMessageList({ messages }: MessageListProps) {
@@ -16,11 +8,11 @@ export default function MyMessageList({ messages }: MessageListProps) {
   return (
     <>
       <h2 className="title-large my-4 text-white">
-        <span className="text-brand-yellow">4</span>개의 메시지를 작성했어요
+        <span className="text-brand-yellow">{messages.length}</span>개의 메시지를 작성했어요
       </h2>
       <section className="flex flex-col gap-4">
         {messages.map((message) => (
-          <MyMessageCard message={TEMP} isShort isOpened />
+          <MyMessageCard message={message} isShort />
         ))}
       </section>
     </>

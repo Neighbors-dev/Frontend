50dd102ac3ebc55bdc6b35493f0b84daca304757
Minha Ahn <dks4857@gmail.com>
2025-01-30 22:27:40 +0900

    Update key.ts

--- src/constants/key.ts
+++ src/constants/key.ts
@@ -1,4 +1,4 @@
-export const SHARE_CODE_KEY = 'TH-SC-qa-5'
-export const REFRESH_TOKEN_KEY = 'TH-RT-qa-5'
-export const MEMBER_INFO_KEY = 'TH-MI-qa-5'
-export const GUEST_INFO_KEY = 'TH-GI-qa-5'
+export const SHARE_CODE_KEY = 'TH-SC-qa-6'
+export const REFRESH_TOKEN_KEY = 'TH-RT-qa-6'
+export const MEMBER_INFO_KEY = 'TH-MI-qa-6'
+export const GUEST_INFO_KEY = 'TH-GI-qa-6'

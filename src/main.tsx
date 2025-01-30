bdb394a2118498beea339f9572cc23f6b8c03027
AAminha <dks4857@gmail.com>
2025-01-30 17:42:18 +0900

    chore: 메타 데이터 추가

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,18 @@ import { BrowserRouter } from 'react-router-dom'
 import App from '@/App'
 import '@/styles/index.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { HelmetProvider } from 'react-helmet-async'
+import MetaTag from './layouts/MetaTag'
 
 const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
-  <QueryClientProvider client={queryClient}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </QueryClientProvider>
+  <HelmetProvider>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <MetaTag />
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
+  </HelmetProvider>
 )

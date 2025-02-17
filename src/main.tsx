67de585dd1c4172361cc58773faaa403c5794d69
AAminha <dks4857@gmail.com>
2025-02-17 21:47:38 +0900

    chore: 메타태그 수정

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import App from '@/App'
 import '@/styles/index.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { HelmetProvider } from 'react-helmet-async'
-import MetaTag from './layouts/MetaTag'
 
 const queryClient = new QueryClient()
 
@@ -12,7 +11,6 @@ createRoot(document.getElementById('root')!).render(
   <HelmetProvider>
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
-        <MetaTag />
         <App />
       </BrowserRouter>
     </QueryClientProvider>

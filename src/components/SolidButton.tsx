09d7deb8915e3bc062e54984c411b461581cf4d2
AAminha <dks4857@gmail.com>
2025-02-17 20:38:57 +0900

    design: 메인 페이지 이미지 설명 추가 및 작성 버튼 플로팅으로 수정

--- src/components/SolidButton.tsx
+++ src/components/SolidButton.tsx
@@ -1,4 +1,4 @@
-import { ComponentPropsWithoutRef } from 'react'
+import { ComponentPropsWithoutRef, forwardRef } from 'react'
 import { twMerge } from 'tailwind-merge'
 
 interface SolidButtonProps extends ComponentPropsWithoutRef<'button'> {
@@ -6,39 +6,39 @@ interface SolidButtonProps extends ComponentPropsWithoutRef<'button'> {
   size: 'small' | 'medium' | 'large'
   className?: string
   children: React.ReactNode
+  ref?: React.RefObject<HTMLButtonElement>
 }
 
-export default function SolidButton({
-  variant,
-  size,
-  className,
-  children,
-  ...props
-}: SolidButtonProps) {
-  // TODO: 색상 - 디자인시스템
+const SolidButton = forwardRef<HTMLButtonElement, SolidButtonProps>(
+  ({ variant, size, className, children, ...props }, ref) => {
+    const variantClass = {
+      primary: 'bg-brand-yellow text-neutral-100 disabled:bg-neutral-80 disabled:text-neutral-90',
+      secondary: 'bg-neutral-80 text-white',
+    }[variant]
 
-  const variantClass = {
-    primary: 'bg-brand-yellow text-neutral-100 disabled:bg-neutral-80 disabled:text-neutral-90',
-    secondary: 'bg-neutral-80 text-white',
-  }[variant]
+    const sizeClass = {
+      large: 'px-6 py-3 label-large-prominent rounded-[10px]',
+      medium: 'px-5 py-2 label-large-prominent rounded-lg',
+      small: 'px-3.5 py-1.5 label-medium-prominent rounded-md',
+    }[size]
 
-  const sizeClass = {
-    large: 'px-6 py-3 label-large-prominent rounded-[10px]',
-    medium: 'px-5 py-2 label-large-prominent rounded-lg',
-    small: 'px-3.5 py-1.5 label-medium-prominent rounded-md',
-  }[size]
+    return (
+      <button
+        ref={ref}
+        className={twMerge(
+          'flex shrink-0 items-center justify-center gap-1.5',
+          variantClass,
+          sizeClass,
+          className
+        )}
+        {...props}
+      >
+        {children}
+      </button>
+    )
+  }
+)
 
-  return (
-    <button
-      className={twMerge(
-        'flex shrink-0 items-center justify-center gap-1.5',
-        variantClass,
-        sizeClass,
-        className
-      )}
-      {...props}
-    >
-      {children}
-    </button>
-  )
-}
+SolidButton.displayName = 'SolidButton'
+
+export default SolidButton

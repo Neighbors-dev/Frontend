6bd6f8c1218a51ffdf26ffe345ea07124651881a
AAminha <dks4857@gmail.com>
2025-04-15 23:59:56 +0900

    chore: Sentry 설정

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,11 @@ import App from '@/App'
 import '@/styles/index.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { HelmetProvider } from 'react-helmet-async'
+import * as Sentry from '@sentry/react'
+
+Sentry.init({
+  dsn: 'https://3d43445cb83fc18b9f27ff7f25d2cb09@o4509157788942336.ingest.us.sentry.io/4509157789925376',
+})
 
 const queryClient = new QueryClient()
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tsconfigPaths from 'vite-tsconfig-paths'
@@ -5,5 +6,12 @@ import svgr from 'vite-plugin-svgr'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), svgr()],
+  plugins: [react(), tsconfigPaths(), svgr(), sentryVitePlugin({
+    org: "neighbor-kn",
+    project: "javascript-react"
+  })],
+
+  build: {
+    sourcemap: true
+  }
 })

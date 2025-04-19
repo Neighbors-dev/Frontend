6352250b01964aea481f5282d2e73208508eb9f9
AAminha <dks4857@gmail.com>
2025-04-20 01:11:05 +0900

    refactor: 캐싱 관련 상수 생성 및 적용

--- src/hooks/useMainData.ts
+++ src/hooks/useMainData.ts
@@ -1,11 +1,12 @@
 import { getMainData } from '@/apis/message'
+import { CACHE_TIME, STALE_TIME } from '@/constants/cache'
 import { useQuery } from '@tanstack/react-query'
 
 export const useMainData = () => {
   return useQuery({
     queryKey: ['main-data'],
     queryFn: () => getMainData(),
-    staleTime: 1000 * 60 * 5, // 5분
-    gcTime: 1000 * 60 * 60, // 1시간
+    staleTime: STALE_TIME,
+    gcTime: CACHE_TIME,
   })
 }
